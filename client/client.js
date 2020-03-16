a6dbab2e8b68e37e030c04afdf81a9687a92db7d
SEARteam1928 <halitulla@gmail.com>
2020-03-16 20:16:56 +0500

    '211181956434582715112203238062733140121842113561193322087931508167768539183246078882169491640211913111331227546229811836240124309590526917'

--- client/client.js
+++ client/client.js
@@ -1,15 +1,80 @@
-var group = localStorage.getItem("group");
+function clock() {
+    var date = new Date(),
+        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+        seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+
+
+    var day = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Воскресенье";
+    weekday[1] = "Понедельник";
+    weekday[2] = "Вторник";
+    weekday[3] = "Среда";
+    weekday[4] = "Четверг";
+    weekday[5] = "Пятница";
+    weekday[6] = "Суббота";
+
+    var mounth = new Array(12);
+    mounth[0] = "янв.";
+    mounth[1] = "фев.";
+    mounth[2] = "мар.";
+    mounth[3] = "апр.";
+    mounth[4] = "май.";
+    mounth[5] = "июн.";
+    mounth[6] = "июл.";
+    mounth[7] = "авг.";
+    mounth[8] = "сен.";
+    mounth[9] = "окт.";
+    mounth[10] = "ноя.";
+    mounth[11] = "дек.";
+
+    document.getElementById("time").innerHTML = hours + ':' + minutes + ":" + seconds;
+    document.getElementById("weekday").innerHTML = weekday[day.getDay()];
+    document.getElementById("date").innerHTML = day.getDate() + " " + mounth[day.getMonth()] + " " + day.getFullYear();
+}
+
+setInterval(clock, 1000);
+
+
+let group = localStorage.getItem("group");
 if (group === null) {
+    if(group === undefined){
+        group = 1;
+    }
     group = 1;
 }
+let groups = {};
 let date = new Date();
 var day = date.getDay();
 if (day === 0) {
-    day = 1;
+    day = 7;
 }
 
+
 const timeLessonMnFr = ["", "08:15\n 09:00", " 09:00\n 09:45", "10:00\n10:45", "10:45\n11:30", "12:00\n12:45", "12:45\n13:30", "14:00\n14:45", "14:45\n15:30", "15:45\n16:30", "16:30\n17:15", "17:25\n18:10", "18:10\n18:55"];
+
 const timeLessonSt = ["", " 8:15\n 9:00", " 9:00\n 9:45", " 9:50\n10:35", "10:35\n11:20", "11:50\n12:35", "12:35\n13:20", "13:30\n14:15", "14:15\n15:00", "15:10\n15:55", "15:55\n16:40", "16:45\n17:30", "17:30\n18:15"];
+
+function updateGroups() {
+    $.ajax({
+        url: "http://timetable.kcpt-1.ru/api/v3/viewAllOptionsForTimetable",
+        type: "get",
+        dataType: "json",
+        data: {},
+        success(data) {
+            console.log(data);
+            groupSelect.innerHTML = "";
+            appendInView(data.groups, 'input', groupSelect, group);
+            updateInfo(data.groups)
+        },
+        error(header, textError) {
+            console.log(header.status, header.statusText);
+            alert("ААА ПАНИКА ПАНИКАААААА");
+        }
+
+    });
+}
 
 function getOptionsList() {
     $.ajax({
@@ -19,10 +84,45 @@ function getOptionsList() {
         data: {},
         success(data) {
             console.log(data);
+            appendInView(data.groups, 'input', groupSelect, group);
+            appendDayOfWeek(data.dayOfWeek, 'div', daySelect, day);
+            updateInfo(data.groups)
+        },
+        error(header, textError) {
+            console.log(header.status, header.statusText);
+            alert("ААА ПАНИКА ПАНИКАААААА");
+        }
 
-            let selectDayOrGroup = [group, day];
-            appendInView(data.groups, 'input', groupSelect, selectDayOrGroup[0]);
-            appendDayOfWeek(data.dayOfWeek, 'div', daySelect, selectDayOrGroup[1]);
+    });
+}
+
+function updateInfo(groups) {
+    infoBox.innerHTML = "";
+
+    for (let i = 0; i < groups.length; i++) {
+        if (groups[i].id.toString() === group) {
+            let infoGroup = document.createElement("div");
+            infoGroup.innerHTML = "<div id='infoGroup' class='infoGroup'>" + groups[i].name.toString() + "</div>";
+            infoBox.append(infoGroup);
+        }
+        if (groups[i].id === group) {
+            let infoGroup = document.createElement("div");
+            infoGroup.innerHTML = "<div id='infoGroup' class='infoGroup'>" + groups[i].name.toString() + "</div>";
+            infoBox.append(infoGroup);
+        }
+    }
+}
+
+function updateDays() {
+    $.ajax({
+        url: "http://timetable.kcpt-1.ru/api/v3/viewAllOptionsForTimetable",
+        type: "get",
+        dataType: "json",
+        data: {},
+        success(data) {
+            console.log(data);
+            daySelect.innerHTML = "";
+            appendDayOfWeek(data.dayOfWeek, 'div', daySelect, day);
         },
         error(header, textError) {
             console.log(header.status, header.statusText);
@@ -38,7 +138,7 @@ function appendInView(arr, createdElement, appendIn, selectDayOrGroups) {
         let element = document.createElement(createdElement);
 
         element.setAttribute("value", arr[i].name);
-        element.setAttribute("onclick", "selectGroup("+arr[i].id+")");
+        element.setAttribute("onclick", "selectGroup(" + arr[i].id + ")");
         element.setAttribute("type", "button");
         element.dataset["id"] = arr[i].id;
         if (arr[i].id.toString() === selectDayOrGroups) {
@@ -56,12 +156,12 @@ function appendDayOfWeek(arr, createdElement, appendIn, selectDayOrGroups) {
     let listDays = [];
     for (let i = 0; i < arr.length; i++) {
         let element = document.createElement(createdElement);
-        element.innerHTML = "<input value=\""+arr[i].short_name+"\" onclick=\"selectDay("+arr[i].id+")\" type=\"button\">";
+        element.innerHTML = "<input value=\"" + arr[i].short_name + "\" onclick=\"selectDay(" + arr[i].id + ")\" type=\"button\">";
         if (arr[i].id !== selectDayOrGroups) {
-            element.innerHTML = "<input class='dayOfWeekNotSelected' value=\""+arr[i].short_name+"\" onclick=\"selectDay("+arr[i].id+")\" type=\"button\">";
+            element.innerHTML = "<input class='dayOfWeekNotSelected' value=\"" + arr[i].short_name + "\" onclick=\"selectDay(" + arr[i].id + ")\" type=\"button\">";
         }
         if (arr[i].id === selectDayOrGroups) {
-            element.innerHTML = "<input class='dayOfWeekSelected' value=\""+arr[i].short_name+"\" onclick=\"selectDay("+arr[i].id+")\" type=\"button\">";
+            element.innerHTML = "<input class='dayOfWeekSelected' value=\"" + arr[i].short_name + "\" onclick=\"selectDay(" + arr[i].id + ")\" type=\"button\">";
         }
         listDays.push(element);
     }
@@ -79,20 +179,18 @@ function updateTimetableList(timetableOn) {
         },
         success(data) {
             console.log(data);
-            console.log("4");
-
             allTable.innerHTML = "";
 
-            userInfo.innerHTML = "";
+/*
+*/
 
 
+/*
             let groupNameDiv = document.createElement("div");
             let dayOfWeek = document.createElement("div");
-            console.log("5");
 
             groupNameDiv.setAttribute("class", "groupName");
             dayOfWeek.setAttribute("class", "dayOfWeek");
-            console.log("6");
 
             if (data.timetable[0] !== undefined) {
                 groupNameDiv.append(data.timetable[0].groupname);
@@ -100,24 +198,15 @@ function updateTimetableList(timetableOn) {
             } else {
                 groupNameDiv.append("Нет занятий!");
             }
-
-            console.log("7");
-
             userInfo.append(groupNameDiv);
             userInfo.append(" ");
             userInfo.append(dayOfWeek);
 
-            console.log("8");
+*/
 
             for (let i = 0; i < data.timetable.length; i++) {
-                console.log("9");
-
                 createTableLine(data, i);
-                console.log("10");
-
             }
-            console.log("11");
-
         },
         error(header, textError) {
             console.log(header.status, header.statusText, textError.toString());
@@ -161,7 +250,7 @@ function createTableLine(data, numSubject) {
         + data.timetable[numSubject].numLesson
         + "</div>"
         + "<div class='subject lessonBlock'>"
-        + data.timetable[numSubject].subgroup +". "+data.timetable[numSubject].subject
+        + data.timetable[numSubject].subgroup + ". " + data.timetable[numSubject].subject
         + " </div>"
         +
         "<div class='teacher lessonBlock'>"
@@ -195,7 +284,7 @@ function createTableLine(data, numSubject) {
                 + "<br>";
         }
     }
-    if (data.timetable[numSubject].subgroup === 1 || data.timetable[numSubject].subgroup === "1")  {
+    if (data.timetable[numSubject].subgroup === 1 || data.timetable[numSubject].subgroup === "1") {
         if (day === "6") {
             dive.innerHTML = subgroupStartStr
                 + "<div class='timeLesson'>" + timeLessonSt[data.timetable[numSubject].numLesson] + "</div>"
@@ -206,9 +295,9 @@ function createTableLine(data, numSubject) {
                 + "<br>";
         }
     }
-    if (data.timetable[numSubject].subgroup === 2 || data.timetable[numSubject].subgroup === "2")  {
+    if (data.timetable[numSubject].subgroup === 2 || data.timetable[numSubject].subgroup === "2") {
         if (day === "6") {
-            dive.innerHTML =subgroupStartStr
+            dive.innerHTML = subgroupStartStr
                 + "<div class='timeLesson'>" + timeLessonSt[data.timetable[numSubject].numLesson] + "</div>"
                 + "<br>";
         } else {
@@ -224,20 +313,21 @@ function selectGroup(opt) {
     localStorage.setItem("group", opt);
     group = opt;
     updateTimetableList("Расписание на ");
+    updateGroups()
 }
 
 function selectDay(opt) {
-    console.log("1");
     day = opt;
-    console.log("2");
     updateTimetableList("Расписание на ");
-    console.log("3");
-
+    updateDays()
 }
 
 getOptionsList();
 updateTimetableList("Расписание на сегодня: ");
 selectDay(day);
+updateGroups();
+clock();
+
 /*window.addEventListener("resize", function () {
     checkScreenSize.innerHTML = innerWidth + "x" + innerHeight;
 }, false);*/
